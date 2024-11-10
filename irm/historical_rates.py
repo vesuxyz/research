4d29843048f709ced466e7429b3f02476b93d014
nbundi <nbundi@proton.me>
2024-11-11 02:20:59 +0700

    Update rates analysis

--- irm/historical_rates.py
+++ irm/historical_rates.py
@@ -1,3 +1,6 @@
+
+#pip install -r requirements.txt # only when running in local mode
+
 import os
 from dotenv import load_dotenv, find_dotenv
 import requests
@@ -46,7 +49,7 @@ while True:
 
 # Decode events from raw data
 events_raw = pd.DataFrame(events_list)
-columns = [2,4,6,8,10,12,14,16,17,18,20,22] # collateral_asset_config (we're not interested in debt asset)
+columns = [2,4,6,8,10,12,14,16,17,18,20,22,44] # collateral_asset_config (we're not interested in debt asset)
 column_names = [
     "collateral_asset",
     "total_collateral_shares",
@@ -59,7 +62,8 @@ column_names = [
     "last_updated",
     "last_rate_accumulator",
     "last_full_utilization_rate",
-    "fee_rate"
+    "fee_rate",
+    "collateral_asset_price"
 ]
 events = events_raw.loc[:,columns]
 events.columns = column_names
@@ -92,16 +96,23 @@ data["debt_dec"] = data["total_nominal_debt"].apply(int, base=16) / SCALE
 data["reserve_dec"] = data["reserve"].apply(int, base=16) / data["scale"].apply(int, base=16)
 data["accumulator_dec"] = data["last_rate_accumulator"].apply(int, base=16) / SCALE
 data["full_rate_dec"] = (1+data["last_full_utilization_rate"].apply(int, base=16) / SCALE)**YEAR - 1
+data["price_dec"] = data["collateral_asset_price"].apply(int, base=16) / SCALE
 
 print("3. Successfully transformed data")
 
 # PART 3: Compute variables of interest
 # -------------------------------------------------------
 
-# Utilization
+# Debt and Total Supplied (native)
 data["debt"] = data["debt_dec"] * data["accumulator_dec"]
-data["totalSupplied"] = data["debt"] + data["reserve_dec"]
-data["utilization"] = (data["debt"] / data["totalSupplied"])*100
+data["supplied"] = data["debt"] + data["reserve_dec"]
+
+# Debt and Total Supplied (USD)
+data["debt_usd"] = data["debt"] * data["price_dec"]
+data["supplied_usd"] = data["supplied"] * data["price_dec"]
+
+# Utilization
+data["utilization"] = (data["debt_usd"] / data["supplied_usd"])*100
 
 # Interest rate (p.a.)
 data = data.sort_values(["timestamp"])
@@ -111,32 +122,76 @@ data["rate_grow"] = data["accumulator_dec"] / data["last_accumulator"]
 data["borrow_rate"] = (data["rate_grow"].pow(YEAR/data["time_diff"])-1)*100
 data['full_rate'] = data['full_rate_dec'] * 100
 
-print("4. Successfully computed variables")
+# Resample data to hourly in order to align time scale across markets
+data_hourly=data.groupby("market").resample("1h",on="date")[["debt_usd","supplied_usd"]].mean().ffill().reset_index(drop=False)
 
-# PART 4: Plot variables
+# Total debt and supplied (USD), and avg utilization across markets
+total_hourly = data_hourly.groupby("date")[["debt_usd","supplied_usd"]].sum()
+total_hourly["utilization"] = (total_hourly["debt_usd"] / total_hourly["supplied_usd"])*100
+
+print("3. Successfully computed variables")
+
+# PART 4: Plot total
 # -------------------------------------------------------
 
-# set date as index for plotting
-data.set_index("date", inplace=True, drop=False)
+# use subset to remove spikes at start and end of sample
+start = total_hourly.reset_index(drop=False).date.min() + pd.DateOffset(1)
+end = total_hourly.reset_index(drop=False).date.max() - pd.DateOffset(1)
+total_sub = total_hourly.query("date>@start and date<@end")
 
-# utilization
 fig, ax = plt.subplots(figsize=(8,6))
-for label, df in data.groupby('market'):
-    df.utilization.plot(ax=ax, label=label)
-
+total_sub.plot.area(
+    ax=ax,
+    y=["supplied_usd","debt_usd"],
+    label=["Supplied","Debt"],
+    stacked=False)
+plt.ylabel("Market Size (USD)")
+total_sub.plot.line(
+    ax=ax,
+    y="utilization",
+    secondary_y=True,
+    label="Utilization")
 plt.xlabel("")
 plt.ylabel("Utilization (%)")
-plt.legend()
-plt.savefig(data.date.min().strftime('%Y-%m-%d') + '_' + data.date.max().strftime('%Y-%m-%d') + '_utilization.png', transparent=False)
+plt.savefig(start.strftime('%Y-%m-%d') + '_' + end.strftime('%Y-%m-%d') + '_total.png', transparent=False)
 
-# annualized borrow rate
-fig, ax = plt.subplots(figsize=(8,6))
-for label, df in data.groupby('market'):
-    df.borrow_rate.plot(ax=ax, label=label)
+print("4. Successfully plotted total")
 
+# PART 5: Plot market share
+# -------------------------------------------------------
+
+# use subset to remove spikes at start and end of sample
+hourly_sub = data_hourly.query("date>@start and date<@end")
+
+# compute market shares
+hourly_sub["total_supplied"] = hourly_sub.groupby("date")["supplied_usd"].transform(sum)
+hourly_sub["market_share"] = hourly_sub["supplied_usd"] / hourly_sub["total_supplied"]                                  
+
+# plot stacked areas
+hourly_sub.pivot(index="date", columns="market", values="market_share").plot(
+    kind="area", stacked=True, figsize=(8,6))
 plt.xlabel("")
-plt.ylabel("Borrow Rate (%)")
-plt.legend()
-plt.savefig(data.date.min().strftime('%Y-%m-%d') + '_' + data.date.max().strftime('%Y-%m-%d') + '_rates.png', transparent=False)
+plt.ylabel("Market Share (%)")
+plt.savefig(start.strftime('%Y-%m-%d') + '_' + end.strftime('%Y-%m-%d') + '_market-share.png', transparent=False)
 
-print("5. Successfully plotted variables")
+print("5. Successfully plotted market share")
+
+# PART 6: Plot markets
+# -------------------------------------------------------
+
+# use subset and resample to remove outliers
+data_sub = data.query("date>@start and date<@end")
+data_sub=data_sub.groupby("market").resample("1h",on="date")[["borrow_rate","utilization"]].max().ffill().reset_index(drop=False)
+
+# plot utilization and rate for each market
+data_sub.set_index("date", inplace=True, drop=False)
+for label, df in data_sub.groupby('market'):
+    ax = df.plot(y="borrow_rate", label="Borrow APR", figsize=(8,6))
+    plt.ylabel("Borrow Rate (%)")
+    df.plot(ax=ax, y="utilization", label="Utilization", secondary_y=True)
+    plt.ylabel("Utilization (%)")
+    plt.title(label)
+    plt.xlabel("")
+    plt.savefig(start.strftime('%Y-%m-%d') + '_' + end.strftime('%Y-%m-%d') + '_' + label + '_.png', transparent=False)
+
+print("6. Successfully plotted markets")
